c7f1a51088d70f8b1b8c83100b55e4f8b0e01149
claudiosfcarvalho <claudiosfcarvalho@outlook.com>
2020-05-28 15:28:25 -0300

    adicionando mais tratamentos no handler

--- socialBooksApi/src/main/java/com/claudioworks/socialBooksApi/handler/ResourceExceptionHandler.java
+++ socialBooksApi/src/main/java/com/claudioworks/socialBooksApi/handler/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.claudioworks.socialBooksApi.Exception.AutorExistenteException;
 import com.claudioworks.socialBooksApi.Exception.AutorNaoEncontradoException;
 import com.claudioworks.socialBooksApi.Exception.LivroNaoEncontradoException;
 import com.claudioworks.socialBooksApi.domain.DetalhesErro;
@@ -36,5 +37,17 @@ public class ResourceExceptionHandler {
 		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/404");
 		erro.setTimestamp(System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+	@ExceptionHandler(AutorExistenteException.class)
+	public ResponseEntity<DetalhesErro> handlerAutorExistenteException
+									(AutorExistenteException e, HttpServletRequest request) {
+		
+		var erro = new DetalhesErro();
+		erro.setStatus(409l);
+		erro.setTitulo(e.getMessage() == null ? "O autor já existe" : e.getMessage());
+		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/409");
+		erro.setTimestamp(System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
 	}
 }
